da9ac270d0e7bc282892c518af6cbd4aa1639b12
Voeffray-Lucielle-EMF <lucielle.voeffray@studentfr.ch>
2024-01-18 14:47:26 +0100

    Ajout du JS

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+function chargement() {
+    alert("Un petit message se cache dans la page, sauras-tu le retrouver ?");
+}
+
+let CatGender = document.getElementById("ImageAlert");
+
+CatGender.addEventListener("click", boop);
+
+function boop() {
+    alert("Boop");
+}
