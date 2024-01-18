e26895f7372bacf2599e8c80405c86da3606c8d4
Voeffray-Lucielle-EMF <lucielle.voeffray@studentfr.ch>
2024-01-18 15:25:19 +0100

    Ajout des commentaires

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
+/*
+Autrice: Lucielle Voeffray
+Date: 15.12.2023
+Description: Code JS pour le projet
+*/
 
+//Alerte au chargement de la page
 function chargement() {
     alert("Un petit message se cache dans la page, sauras-tu le retrouver ?");
 }
 
+//Alert si le message a été trouvé (cliquer sur le drapeau catgender)
 let CatGender = document.getElementById("ImageAlert");
 
 CatGender.addEventListener("click", boop);
