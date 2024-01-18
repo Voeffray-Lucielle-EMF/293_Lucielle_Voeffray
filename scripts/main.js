5ee20af48199fb00cc689420b7d87df6b5b6012f
Voeffray-Lucielle-EMF <lucielle.voeffray@studentfr.ch>
2024-01-18 16:30:28 +0100

    Automatisation de la date de modification

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
 /*
 Autrice: Lucielle Voeffray
-Date: 15.12.2023
+Date: 18.01.2024
 Description: Code JS pour le projet
 */
+
+let LastMod = "18.01.2024"
 
 //Alerte au chargement de la page
 function chargement() {
     alert("Un petit message se cache dans la page, sauras-tu le retrouver ?");
+    document.getElementById("date").textContent = "Dernière mise à jour: " + LastMod;
 }
 
 //Alert si le message a été trouvé (cliquer sur le drapeau catgender)
@@ -17,3 +20,4 @@ CatGender.addEventListener("click", boop);
 function boop() {
     alert("Boop");
 }
+
