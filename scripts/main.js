11111e53b0209ca8858a3df3996367e6417b0155
Voeffray-Lucielle-EMF <lucielle.voeffray@studentfr.ch>
2024-01-19 08:21:58 +0100

    Ajout du scipt dans toutes les pages

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,17 @@ Date: 18.01.2024
 Description: Code JS pour le projet
 */
 
+//Changer la date de modification
 let LastMod = "18.01.2024"
 
+let date = document.getElementById("date")
+date.textContent = "Dernière mise à jour: " + LastMod;
+console.log(date.textContent = "Dernière mise à jour: " + LastMod);
+
 //Alerte au chargement de la page
-function chargement() {
+/* function chargement() {
     alert("Un petit message se cache dans la page, sauras-tu le retrouver ?");
-    document.getElementById("date").textContent = "Dernière mise à jour: " + LastMod;
-}
+} */
 
 //Alert si le message a été trouvé (cliquer sur le drapeau catgender)
 let CatGender = document.getElementById("ImageAlert");
